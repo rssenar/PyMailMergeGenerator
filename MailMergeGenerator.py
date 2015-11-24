7828756e42e9fc4547bc24cecb50be04f7544ba8
Richard Senar <rssenar@richards-imac.attlocal.net>
2015-11-23 16:44:03 -0800

    Updated Comments

--- MailMergeGenerator.py
+++ MailMergeGenerator.py
@@ -1,21 +1,28 @@
 '''
-ooo        ooooo            o8o  oooo  ooo        ooooo                                           .oooooo.                                                           .                      
-`88.       .888'            `"'  `888  `88.       .888'                                          d8P'  `Y8b                                                        .o8                      
- 888b     d'888   .oooo.   oooo   888   888b     d'888   .ooooo.  oooo d8b  .oooooooo  .ooooo.  888            .ooooo.  ooo. .oo.    .ooooo.  oooo d8b  .oooo.   .o888oo  .ooooo.  oooo d8b 
- 8 Y88. .P  888  `P  )88b  `888   888   8 Y88. .P  888  d88' `88b `888""8P 888' `88b  d88' `88b 888           d88' `88b `888P"Y88b  d88' `88b `888""8P `P  )88b    888   d88' `88b `888""8P 
- 8  `888'   888   .oP"888   888   888   8  `888'   888  888ooo888  888     888   888  888ooo888 888     ooooo 888ooo888  888   888  888ooo888  888      .oP"888    888   888   888  888     
- 8    Y     888  d8(  888   888   888   8    Y     888  888    .o  888     `88bod8P'  888    .o `88.    .88'  888    .o  888   888  888    .o  888     d8(  888    888 . 888   888  888     
-o8o        o888o `Y888""8o o888o o888o o8o        o888o `Y8bod8P' d888b    `8oooooo.  `Y8bod8P'  `Y8bood8P'   `Y8bod8P' o888o o888o `Y8bod8P' d888b    `Y888""8o   "888" `Y8bod8P' d888b    
-                                                                           d"     YD                                                                                                        
-                                                                           "Y88888P'                                                                                                        
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ |M|a|i|l|M|e|r|g|e|G|e|n|e|r|a|t|o|r|
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 '''
 #!/usr/bin/python3.4.3
-# ---------------------------------------------
 # Imports
-# ---------------------------------------------
 import csv
 # ---------------------------------------------
-# Global Variables
+'''
+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+
+ |G|l|o|b|a|l| |V|a|r|i|a|b|l|e|s|
+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+
+'''
+# Column[0] = City
+# Column[1] = State
+# Column[2] = Zip
+# Column[3] = Crrt
+# Column[4] = Route Count
+# Column[5] = Bundle
+# Column[6] = Bundle Per Route
+# Column[7] = Last bundle
+# Column[8] = SeqNum (Output Field, Set to 0)
+# Column[9] = FinalBundles (Output Field, Set to 0)
+# Column[10] = Count (Output Field, Set to 0)
 # ---------------------------------------------
 CSVFilesHaveHeaderRow = True # True or False if input files include a header row
 InputFile = "Input.csv"
@@ -28,11 +35,15 @@ SeqNum = 8
 UpdatedBundles = 9 
 Count = 10 
 # ----------------------------------------------
-# Objects
-# ----------------------------------------------
+''' 
+ +-+-+-+-+-+-+-+
+ |O|b|j|e|c|t|s|
+ +-+-+-+-+-+-+-+ 
+'''
 old = csv.reader(open(InputFile,'r'))
 new = csv.writer(open(OutputFile,'a'))
-new.writerow(['City','State','Zip','Crrt','RouteCount','Bundle','BundlePerRoute','LastBundle','SeqNum','FinalBundles','Count'])
+new.writerow(['City','State','Zip','Crrt','RouteCount','Bundle','BundlePerRoute',\
+	'LastBundle','SeqNum','FinalBundles','Count'])
 # ----------------------------------------------
 # Main Program
 # ----------------------------------------------
@@ -56,3 +67,4 @@ for line in old:
 			line[Count] = str(line[SeqNum]) + " of " + str(line[BundlePerRoute])
 			new.writerow(line)
 			counter+=1
+
